c61036aef86b64824c67777c86386b78b1ab9976
elizebethrinklesunny <charusunny124@gmail.com>
2024-10-29 15:07:41 +0530

    skills

--- src/App.js
+++ src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './components/Navbar';
 import Banner from './components/Banner';
+import Skills from './components/Skills';
 
 function App() {
   return (
     <div className="App">
      <Navbar/>
      <Banner/>
+     <Skills/>
     </div>
   );
 }
